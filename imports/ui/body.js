feea64f7b66804a3f83d228922d71ddf3e23385d
Nabil Hassein <nabil.hassein@gmail.com>
2018-06-27 10:41:52 -0400

    chapter 8: user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -32,6 +33,8 @@ Template.body.events({
     Posts.insert({
       text: text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
     target.text.value = '';
